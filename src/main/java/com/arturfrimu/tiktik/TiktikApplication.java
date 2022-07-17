01b8790c979bc92af4edfa0fcf8ae715c8bc8362
mmaria159 <macovei.maria1999@gmail.com>
2022-07-17 18:50:34 +0300

    Initial commit

---
+++ src/main/java/com/arturfrimu/tiktik/TiktikApplication.java
@@ -0,0 +1,11 @@
+package com.arturfrimu.tiktik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktikApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TiktikApplication.class, args);
+    }
+}
